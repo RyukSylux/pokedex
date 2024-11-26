f50174322a982a7ed91c8192b8ba169dbec05c3a
RyukSylux <49920984+RyukSylux@users.noreply.github.com>
2024-11-26 01:06:02 +0100

    Revert "Meilleure searchbar"

--- Form1.cs
+++ Form1.cs
@@ -272,7 +272,7 @@ namespace PokedexApp
             if (txtName.Text != null)
             {
                 string search = txtName.Text;
-                var filteredPokemons = pokemons.FindAll(p => p.Name.Contains(search));
+                var filteredPokemons = pokemons.FindAll(p => p.Name == search);
 
                 listBoxPokemons.DataSource = null; // Réinitialise la source de données
                 listBoxPokemons.DataSource = filteredPokemons;
--- obj/Debug/net8.0-windows/PokedexApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PokedexApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6864e5d9e32b4e80d7acb4c3521ea03c03326545")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
